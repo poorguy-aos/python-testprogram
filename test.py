64560124cd1ebeaa13b7612f08bb37a9ba254844
vito <vito@ubuntu.org>
2019-01-20 18:47:59 +0800

    V1

--- test.py
+++ test.py
@@ -10,31 +10,30 @@ class ColorWeight:
         with open('trees-data.txt', 'r') as data:
             combos = data.readlines()[-1].split(',')
             if days == 7:
-                combos[self.number] = str(int(combos[self.number]) + 1)
-                self.combos = combos
-                return int(combos[self.number])
+                self.combos= str(int(combos[self.number]) + 1)
+                return int(self.combos)
             else:
-                combos[self.number] = '0'
-                self.combos = combos
+                self.combos = '0'
                 return 0
-        
+
+    def reward_trees(self, days):
+        '''you must rewrite this function!'''
+        return 0
+
     def obtain_trees(self, days):
         '''calculate the number of trees'''
         tree = self.weight - (7 - days)
         if tree <= 0:
             tree = 0
+        tree += self.reward_trees(days)
         return tree
-    
-    def reward_trees(self, days):
-        '''you must rewrite this function!'''
-        return 0
-    
-    def calculate_tree(self, days):
-        self.trees = self.obtain_trees(days) + self.reward_trees(days)
-        with open('trees-data.txt', 'a') as data:
-            self.combos[0] = str(int(self.combos[0]) + self.trees)
-            data.writelines(','.join(self.combos))
-        return self.trees
+
+    # def calculate_tree(self, days):
+    #     self.trees = self.obtain_trees(days) + self.reward_trees(days)
+    #     # with open('trees-data.txt', 'a') as data:
+    #     #     self.combos[0] = str(int(self.combos[0]) + self.trees)
+    #     #     data.writelines(','.join(self.combos))
+    #     return self.trees
 
 class BlueWeight(ColorWeight):
     def __init__(self, number=-2):
@@ -43,7 +42,7 @@ class BlueWeight(ColorWeight):
         self.number = number
         self.reward_weight = [0, 1, 2, 4, 7, 11, 16]
         if number == -2:
-            raise 'you must give squence number!'
+            raise Exception('you must give squence number!')
     def reward_trees(self, days):
         temp = self.Combos_Weeks(days)
         if temp-1 >= 5:
@@ -60,7 +59,7 @@ class Yellow_Weiht(ColorWeight):
         self.weight = 4
         self.number = number
         if number == -2:
-            raise 'you must give squence number!'
+            raise Exception('you must give squence number!')
     def reward_trees(self, days):
         temp = self.Combos_Weeks(days)
         if temp <= 1:
@@ -77,7 +76,7 @@ class Red_Weight(ColorWeight):
         self.weight = 6
         self.number = number
         if number == -2:
-            raise 'you must give squence number!'
+            raise Exception('you must give squence number!')
     def reward_trees(self, days):
         pass
 
@@ -87,18 +86,28 @@ bt = BlueWeight(3)
 ml = BlueWeight(4)
 gb = Yellow_Weiht(5)
 
-tree1 = ge.calculate_tree(3)
-tree2 = hb.calculate_tree(7)
-tree3 = bt.calculate_tree(7)
-tree4 = ml.calculate_tree(7)
-tree5 = gb.calculate_tree(7)
+all_color = [ge, hb, bt, ml, gb]
+all_add = []
 
-all_add = [tree1,tree2,tree3,tree4,tree5]
+# for i, cls in enumerate(all_color):
+#     input_days = int(input("please input a number of a days"))
+#     all_add[i] = cls.obtain_trees(input_days)
+
+tree1 = ge.obtain_trees(4)
+tree2 = hb.obtain_trees(7)
+tree3 = bt.obtain_trees(7)
+tree4 = ml.obtain_trees(7)
+tree5 = gb.obtain_trees(7)
+
+all_add = [tree1, tree2, tree3, tree4, tree5]
 print(all_add)
-final_trees = sum(all_add) + 6 - 30
+final_trees = sum(all_add) + 3 - 30
 with open('trees-data.txt', 'r') as data:
     temp = data.readlines()[-1].split(',')
     temp[0] = str(int(temp[0]) + final_trees)
-    print(','.join(temp))
+
 with open('trees-data.txt', 'a') as data:
+    for i in range(5):
+        temp[i+1] = all_color[i].combos
+    print(','.join(temp))
     data.writelines(','.join(temp))
