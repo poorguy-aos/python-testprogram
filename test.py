43b7c05f0638ac19db6e31b65450e3ae2bf8bfd9
aos <aos@cloud.com>
2019-10-29 20:14:13 +0800

    temp new version

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
 class ColorWeight:
     '''颜色权重，用于显色定义权重'''
+
     def __init__(self):
         self.weight = 1
         self.trees = 0
         self.combos = None
-    def Combos_Weeks(self, days=None):
-        '''连续坚持额外奖励tree
+
+    def Combos_Weeks(self, preCombos, days=None):
+        '''传入历史数据preCombos和days，输出新的Combos数
         此处改为返回连续天数更为合适'''
-        with open('trees-data.txt', 'r') as data:
-            combos = data.readlines()[-1].split(',')
-            if days == 7:
-                self.combos= str(int(combos[self.number]) + 1)
-                return int(self.combos)
-            else:
-                self.combos = '0'
-                return 0
+        if days == 7:
+            self.combos = str(int(preCombos) + 1)
+            return int(self.combos)
+        else:
+            self.combos = '0'
+            return 0
 
     def reward_trees(self, days):
         '''you must rewrite this function!'''
         return 0
 
-    def obtain_trees(self, days):
+    def obtain_trees(self, days, preCombos):
         '''calculate the number of trees'''
         tree = self.weight - (7 - days)
         if tree <= 0:
             tree = 0
-        tree += self.reward_trees(days)
+        tree += self.reward_trees(days, preCombos)
         return tree
 
     # def calculate_tree(self, days):
@@ -35,6 +35,7 @@ class ColorWeight:
     #     #     data.writelines(','.join(self.combos))
     #     return self.trees
 
+
 class BlueWeight(ColorWeight):
     def __init__(self, number=-2):
         super().__init__()
@@ -43,8 +44,9 @@ class BlueWeight(ColorWeight):
         self.reward_weight = [0, 1, 2, 4, 7, 11, 16]
         if number == -2:
             raise Exception('you must give squence number!')
-    def reward_trees(self, days):
-        temp = self.Combos_Weeks(days)
+
+    def reward_trees(self, days, preCombos):
+        temp = self.Combos_Weeks(days, preCombos)
         if temp-1 >= 5:
             return 10
         elif temp == 0:
@@ -60,25 +62,30 @@ class Yellow_Weiht(ColorWeight):
         self.number = number
         if number == -2:
             raise Exception('you must give squence number!')
-    def reward_trees(self, days):
-        temp = self.Combos_Weeks(days)
+
+    def reward_trees(self, days, preCombos):
+        temp = self.Combos_Weeks(days, preCombos)
         if temp <= 1:
             return 0
         elif 2 ** (temp - 1) >= 10:
             return 10
         else:
-            return 2 ** (temp -1)
+            return 2 ** (temp - 1)
+
 
 class Red_Weight(ColorWeight):
     '''Red_Weight too complex still wait to finish'''
+
     def __init__(self, number=-2):
         super().__init__()
         self.weight = 6
         self.number = number
         if number == -2:
             raise Exception('you must give squence number!')
+
     def reward_trees(self, days):
         pass
+
 
 ge = Yellow_Weiht(1)
 hb = BlueWeight(2)
@@ -88,26 +95,15 @@ gb = Yellow_Weiht(5)
 
 all_color = [ge, hb, bt, ml, gb]
 all_add = []
-
-# for i, cls in enumerate(all_color):
-#     input_days = int(input("please input a number of a days"))
-#     all_add[i] = cls.obtain_trees(input_days)
-
-tree1 = ge.obtain_trees(6)
-tree2 = hb.obtain_trees(7)
-tree3 = bt.obtain_trees(6)
-tree4 = ml.obtain_trees(1)
-tree5 = gb.obtain_trees(7)
-
-all_add = [tree1, tree2, tree3, tree4, tree5]
-print(all_add)
-final_trees = sum(all_add) - 22  - 20
 with open('trees-data.txt', 'r') as data:
-    temp = data.readlines()[-1].split(',')
-    temp[0] = str(int(temp[0]) + final_trees)
+    last_combos = data.readlines()[-1].split(',')
+    for i, colori in enumerate(all_color):
+            day = int(input("please input days:"))
+            all_add.append(colori.obtain_trees(days, last_combos[i+1]))
+    last_combos[0] = str(int(last_combos[0]) + sum(all_add) - 22 - 20)
 
 with open('trees-data.txt', 'a') as data:
     for i in range(5):
-        temp[i+1] = all_color[i].combos
-    print(','.join(temp))
-    data.writelines(','.join(temp))
+        last_combos[i+1] = all_color[i].combos
+    print(','.join(last_combos))
+    data.writelines(','.join(last_combos))
